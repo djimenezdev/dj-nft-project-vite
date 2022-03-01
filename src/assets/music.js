78d92f13dbc0b51a2f41a0d64088bffa6ac87b0e
Daniel <42664403+danieljimenez0255@users.noreply.github.com>
2022-03-01 12:29:33 -0800

    assets

---
+++ src/assets/music.js
@@ -0,0 +1,27 @@
+import campusAudio from "./campus-low-fi.mp3";
+import campusImg from "./campus-low-fi-img.jpg";
+import cantAudio from "./cant-get-enough.mp3";
+import cantImg from "./cant-get-enough-img.jpg";
+import breatheAudio from "./just-breathe.mp3";
+import breatheImg from "./just-breathe-img.jpg";
+
+export const musicData = [
+  {
+    beat: campusAudio,
+    title: "Philanthrope - Campus",
+    image: campusImg,
+    source: "https://www.youtube.com/watch?v=am-Yx_gotig",
+  },
+  {
+    beat: cantAudio,
+    title: "Montell - Can't Get Enough",
+    image: cantImg,
+    source: "https://www.youtube.com/watch?v=XDd5vyxGAII",
+  },
+  {
+    beat: breatheAudio,
+    title: "Bcalm & Banks - Just Breathe",
+    image: breatheImg,
+    source: "https://www.youtube.com/watch?v=fktpxyIhZMI",
+  },
+];
